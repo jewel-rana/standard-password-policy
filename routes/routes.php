d7bd23898860aed824c26fc4220fbdc87dd2176b
Jewel Rana <jewelrana.dev@gmail.com>
2025-01-02 13:35:07 +0600

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -5,7 +5,7 @@ use JewelRana\PasswordPolicy\Http\Controllers\ResetPasswordController;
 
 Route::middleware('web')->group(function () {
     Route::middleware('auth')->prefix('password')->group(function () {
-        Route::get('reset', [ResetPasswordController::class, 'resetForm'])->name('password-policy.form');
-        Route::post('reset', [ResetPasswordController::class, 'resetPassword'])->name('password-policy.reset');
+        Route::get('standard/reset', [ResetPasswordController::class, 'resetForm'])->name('password-policy.form');
+        Route::post('standard/reset', [ResetPasswordController::class, 'resetPassword'])->name('password-policy.reset');
     });
 });
