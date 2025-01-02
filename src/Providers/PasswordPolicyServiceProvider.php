51e3b15a3df7fc876d1271350c6f450f8de10f94
Jewel Rana <jewelrana.dev@gmail.com>
2025-01-02 14:33:49 +0600

    Update PasswordPolicyServiceProvider.php

--- src/Providers/PasswordPolicyServiceProvider.php
+++ src/Providers/PasswordPolicyServiceProvider.php
@@ -15,6 +15,11 @@ class PasswordPolicyServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../../config/password-policy.php' =>  config_path('password-policy.php'),
         ], 'config');
+        
+        // Publish views to the application's views directory
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/password-policy'),
+        ], 'views');
         if(class_exists('App\\Models\\User')) {
             \App\Models\User::observe(UserModelObserver::class);
         }
