867f849995fc744e987821e4f0cea13b15c291ee
Jewel Rana <jewelrana.dev@gmail.com>
2025-01-05 16:55:43 +0600

    Update ResetPasswordService.php

--- src/Services/ResetPasswordService.php
+++ src/Services/ResetPasswordService.php
@@ -4,12 +4,24 @@ namespace JewelRana\PasswordPolicy\Services;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use App\Traits\EnsureSecurityTrait;
 
 class ResetPasswordService
 {
+    use EnsureSecurityTrait;
+    
     public function resetPassword(array $data)
     {
         $user = User::first();
+        if($this->hasAlreadyBlocked($user->email)) {
+                auth()->logout();
+                return redirect()->route('auth.login')->with([
+                    'message' => [
+                        'label' => 'info',
+                        'content' => __('Your account has blocked due to multiple failed attempts.')
+                    ]
+                ]);
+        }
         $user->update(['password' => bcrypt($data['password'])]);
         auth()->login($user);
         Auth::logoutOtherDevices($data['password']);
