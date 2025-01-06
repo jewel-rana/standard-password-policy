dcdb21aa149777be255c5e7fa7a51272b7a084d6
Jewel Rana <jewelrana.dev@gmail.com>
2025-01-06 14:03:02 +0600

    Update PasswordPolicyExpired.php

--- src/Http/Middleware/PasswordPolicyExpired.php
+++ src/Http/Middleware/PasswordPolicyExpired.php
@@ -22,12 +22,21 @@ class PasswordPolicyExpired
             $user = $request->user();
             $lastChanged = UserPassword::where('user_id', $user->id)->latest()->first();
             $password_changed_at = new Carbon($lastChanged->created_at ?? $user->created_at);
-    
-            if (Carbon::now()->diffInDays($password_changed_at) >= (config('password-policy.max_expire_days') ?? 30)) {
-                session()->flash('message', 'Your password has expired. please reset it');
-                return redirect()->route('password-policy.form');
+            $dateDiff = Carbon::now()->diffInDays($password_changed_at);
+            $expireDays = (config('password-policy.max_expire_days', 30));
+            $alertDays = (config('password-policy.expire_alert_days', 25));
+            $remainingDays = $expireDays - $dateDiff;
+            if ($dateDiff >= $alertDays && $dateDiff <= $expireDays) {
+                session()->flash('alertMessage', "Your password are about to expire within {$remainingDays} days. Please <a href='" . route('change-password'). "'><strong>change</strong></a> your password.");
+            }
+
+            if($dateDiff >= $expireDays) {
+                session()->flash('message', 'Your password has expire please reset your password.');
+                return redirect()->route('password-policy.reset')
+                    ->with(['message' => ['label' => 'info', 'content' => 'Your password has expired please reset your password.']]);
             }
         }
         return $next($request);
     }
 }
+
