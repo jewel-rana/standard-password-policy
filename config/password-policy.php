f4e6384dcc9348fda95f194d6342c63895f0eee3
Jewel Rana <jewelrana.dev@gmail.com>
2025-01-06 14:03:24 +0600

    Update password-policy.php

--- config/password-policy.php
+++ config/password-policy.php
@@ -1,4 +1,5 @@
 <?php
 return [
-    'max_expire_days' => 30
+    'max_expire_days' => env('USER_ACCESS_PASSWORD_EXPIRY_PERIOD', 30),
+    'expire_alert_days' => env('USER_ACCESS_PASSWORD_EXPIRY_ALERT_PERIOD', 25),
 ];
